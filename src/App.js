f99db573154cfabc28307137eaf3558b9a8a0bc9
macandelachaves <149009525+macandelachaves@users.noreply.github.com>
2024-10-28 20:07:27 +0000

    solved - deploy bug link

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ export default function App() {
           <a
             href="https://github.com/macandelachaves/react-week5-homework"
             target="_blank"
+            rel="noreferrer"
           >
             Github
           </a>{" "}
